14011e164ab942b41697c8da4e1ef487ffaccd87
Scott Beck <sbeck@shambhalamountain.org>
2017-12-13 17:30:24 -0700

    working on firebase delete now

--- build/bundle.js
+++ build/bundle.js
@@ -54,7 +54,14 @@ module.exports =
 	var app = express();
 	var Request = __webpack_require__(14);
 	var memoizer = __webpack_require__(15);
-	var Error = __webpack_require__(16);
+	var NestedError = __webpack_require__(16);
+	var crypto = __webpack_require__(17);
+
+	function md5(str, raw) {
+	  var hash = crypto.createHash('md5').update(str);
+	  if (raw) return hash.digest();
+	  return hash.digest('hex');
+	}
 
 	function lastLogCheckpoint(req, res) {
 	  var ctx = req.webtaskContext;
@@ -68,6 +75,17 @@ module.exports =
 	    return res.status(400).send({ message: 'Missing settings: ' + missing_settings.join(', ') });
 	  }
 
+	  var secretKey;
+	  try {
+	    secretKey = fbadmin.credential.cert(JSON.parse(ctx.data.FIREBASE_SECRET_KEY));
+	  } catch (err) {
+	    console.log("Error parsing FIREBASE_SECRET_KEY json: ", err);
+	    return callback(new NestedError('Error parsing FIREBASE_SECRET_KEY json: ', err));
+	  }
+	  fbadmin.initializeApp({
+	    credential: secretKey
+	  });
+	  secretKey = null;
 	  // If this is a scheduled task, we'll get the last log checkpoint from the previous run and continue from there.
 	  req.webtaskContext.storage.get(function (err, data) {
 	    if (err && err.output.statusCode !== 404) return res.status(err.code).send(err);
@@ -103,24 +121,10 @@ module.exports =
 
 	      getLogs({ checkpointId: startCheckpointId });
 	    }, function (context, callback) {
-	      var min_log_level = 1;
-	      var log_matches_level = function log_matches_level(log) {
-	        if (logTypes[log.type]) {
-	          return logTypes[log.type].level >= min_log_level;
-	        }
-	        return true;
-	      };
 	      // sdu successful user deletion
-	      // sce successful email change
-	      var types_filter = ['du', 'ss', 'sdu'];
-	      var log_matches_types = function log_matches_types(log) {
-	        console.log("Log: " + JSON.stringify(log));
-	        return log.type && types_filter.indexOf(log.type) >= 0;
-	      };
-
-	      context.logs = context.logs
-	      //.filter(l => l.type !== 'sapi' && l.type !== 'fapi')
-	      .filter(log_matches_level).filter(log_matches_types);
+	      context.logs = context.logs.filter(function (l) {
+	        return l.type === 'sdu' || l.type == 'ss';
+	      });
 
 	      callback(null, context);
 	    }, function (context, callback) {
@@ -129,23 +133,44 @@ module.exports =
 	        return callback(null, context);
 	      }
 
-	      var concurrent_calls = 1;
-
-	      var secretKey;
-	      try {
-	        secretKey = fbadmin.credential.cert(JSON.parse(ctx.data.FIREBASE_SECRET_KEY));
-	      } catch (err) {
-	        console.log("Error parsing FIREBASE_SECRET_KEY json: ", err);
-	        return callback(new NestedError('Error parsing FIREBASE_SECRET_KEY json: ', err));
-	      }
-	      fbadmin.initializeApp({
-	        credential: secretKey
-	      });
-
-	      async.eachLimit(context.logs, concurrent_calls, function (log, cb) {
+	      context.userDeleteMatches = {};
+	      var errors = [];
+	      context.logs.forEach(function (log) {
 	        console.log("Log: ", log);
-	        cb();
+	        var userId = log.description.split(/:\s+/, 1)[1];
+	        if (!userId) {
+	          console.log("Missing description from log entry: ", log);
+	          errors.push("Missing userId from description: " + log.description);
+	        } else if (log.type === 'sdu') {
+	          context.userDeleteMatches[userId] = true;
+	        } else if (log.type === 'ss') {
+	          delete context.userDeleteMatches[userId];
+	        }
+	      });
+	      callback(err, context);
+	    }, function (context, callback) {
+	      var concurrent_calls = 3;
+	      var errors = [];
+
+	      if (context.userDeleteMatches.keys().length === 0) return callback(null, context);
+
+	      console.log("UserDeleteMatches: ", context.userDeleteMatches);
+	      async.eachLimit(context.userDeleteMatches.keys(), concurrent_calls, function (userId, cb) {
+	        userExists(userId, function (err, email) {
+	          if (err) {
+	            errors.push("" + err);
+	          }
+	          if (!email && !err) {
+	            deleteFirebaseUser(userId, function (err) {
+	              if (err) {
+	                errors.push("" + err);
+	              }
+	              cb();
+	            });
+	          }
+	        });
 	      }, function (err) {
+	        if (!err && errors) err = new Error(errors.join("; "));
 	        callback(err, context);
 	      });
 	    }], function (err, context) {
@@ -181,191 +206,38 @@ module.exports =
 	  });
 	}
 
-	var logTypes = {
-	  's': {
-	    event: 'Success Login',
-	    level: 1 // Info
-	  },
-	  'seacft': {
-	    event: 'Success Exchange',
-	    level: 1 // Info
-	  },
-	  'seccft': {
-	    event: 'Success Exchange (Client Credentials)',
-	    level: 1 // Info
-	  },
-	  'feacft': {
-	    event: 'Failed Exchange',
-	    level: 3 // Error
-	  },
-	  'feccft': {
-	    event: 'Failed Exchange (Client Credentials)',
-	    level: 3 // Error
-	  },
-	  'f': {
-	    event: 'Failed Login',
-	    level: 3 // Error
-	  },
-	  'w': {
-	    event: 'Warnings During Login',
-	    level: 2 // Warning
-	  },
-	  'du': {
-	    event: 'Deleted User',
-	    level: 1 // Info
-	  },
-	  'fu': {
-	    event: 'Failed Login (invalid email/username)',
-	    level: 3 // Error
-	  },
-	  'fp': {
-	    event: 'Failed Login (wrong password)',
-	    level: 3 // Error
-	  },
-	  'fc': {
-	    event: 'Failed by Connector',
-	    level: 3 // Error
-	  },
-	  'fco': {
-	    event: 'Failed by CORS',
-	    level: 3 // Error
-	  },
-	  'con': {
-	    event: 'Connector Online',
-	    level: 1 // Info
-	  },
-	  'coff': {
-	    event: 'Connector Offline',
-	    level: 3 // Error
-	  },
-	  'fcpro': {
-	    event: 'Failed Connector Provisioning',
-	    level: 4 // Critical
-	  },
-	  'ss': {
-	    event: 'Success Signup',
-	    level: 1 // Info
-	  },
-	  'fs': {
-	    event: 'Failed Signup',
-	    level: 3 // Error
-	  },
-	  'cs': {
-	    event: 'Code Sent',
-	    level: 0 // Debug
-	  },
-	  'cls': {
-	    event: 'Code/Link Sent',
-	    level: 0 // Debug
-	  },
-	  'sv': {
-	    event: 'Success Verification Email',
-	    level: 0 // Debug
-	  },
-	  'fv': {
-	    event: 'Failed Verification Email',
-	    level: 0 // Debug
-	  },
-	  'scp': {
-	    event: 'Success Change Password',
-	    level: 1 // Info
-	  },
-	  'fcp': {
-	    event: 'Failed Change Password',
-	    level: 3 // Error
-	  },
-	  'sce': {
-	    event: 'Success Change Email',
-	    level: 1 // Info
-	  },
-	  'fce': {
-	    event: 'Failed Change Email',
-	    level: 3 // Error
-	  },
-	  'scu': {
-	    event: 'Success Change Username',
-	    level: 1 // Info
-	  },
-	  'fcu': {
-	    event: 'Failed Change Username',
-	    level: 3 // Error
-	  },
-	  'scpn': {
-	    event: 'Success Change Phone Number',
-	    level: 1 // Info
-	  },
-	  'fcpn': {
-	    event: 'Failed Change Phone Number',
-	    level: 3 // Error
-	  },
-	  'svr': {
-	    event: 'Success Verification Email Request',
-	    level: 0 // Debug
-	  },
-	  'fvr': {
-	    event: 'Failed Verification Email Request',
-	    level: 3 // Error
-	  },
-	  'scpr': {
-	    event: 'Success Change Password Request',
-	    level: 0 // Debug
-	  },
-	  'fcpr': {
-	    event: 'Failed Change Password Request',
-	    level: 3 // Error
-	  },
-	  'fn': {
-	    event: 'Failed Sending Notification',
-	    level: 3 // Error
-	  },
-	  'sapi': {
-	    event: 'API Operation'
-	  },
-	  'limit_wc': {
-	    event: 'Blocked Account',
-	    level: 4 // Critical
-	  },
-	  'limit_ui': {
-	    event: 'Too Many Calls to /userinfo',
-	    level: 4 // Critical
-	  },
-	  'api_limit': {
-	    event: 'Rate Limit On API',
-	    level: 4 // Critical
-	  },
-	  'sdu': {
-	    event: 'Successful User Deletion',
-	    level: 1 // Info
-	  },
-	  'fdu': {
-	    event: 'Failed User Deletion',
-	    level: 3 // Error
-	  },
-	  'fapi': {
-	    event: 'Failed API Operation',
-	    level: 3 // Error
-	  },
-	  'limit_mu': {
-	    event: 'Blocked IP Address',
-	    level: 3 // Error
-	  },
-	  'slo': {
-	    event: 'Success Logout',
-	    level: 1 // Info
-	  },
-	  'flo': {
-	    event: ' Failed Logout',
-	    level: 3 // Error
-	  },
-	  'sd': {
-	    event: 'Success Delegation',
-	    level: 1 // Info
-	  },
-	  'fd': {
-	    event: 'Failed Delegation',
-	    level: 3 // Error
-	  }
-	};
+	function deleteFirebaseUser(userId, cb) {
+	  console.log("Delete Firbase User " + userId);
+	  fbadmin.auth().deleteUser(userId).then(function () {
+	    return cb(null);
+	  }).catch(function (e) {
+	    return cb(e);
+	  });
+	}
+
+	function userExits(userId, cb) {
+	  var url = 'https://' + domain + '/api/v2/users/' + userId;
+
+	  Request({
+	    method: 'GET',
+	    url: url,
+	    json: true,
+	    qs: {
+	      fields: 'email'
+	    },
+	    headers: {
+	      Authorization: 'Bearer ' + token,
+	      Accept: 'application/json'
+	    }
+	  }, function (err, res, body) {
+	    if (err) {
+	      console.log("Error getting user " + userId + ": ", err);
+	      cb(null, false);
+	    } else {
+	      cb(null, true);
+	    }
+	  });
+	}
 
 	function getPageOfLogsFromAuth0(domain, token, take, from, cb) {
 	  var url = 'https://' + domain + '/api/v2/logs';
@@ -1123,6 +995,12 @@ module.exports =
 /***/ (function(module, exports) {
 
 	module.exports = require("nested-error-stacks");
+
+/***/ }),
+/* 17 */
+/***/ (function(module, exports) {
+
+	module.exports = require("crypto");
 
 /***/ })
 /******/ ]);
--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const Webtask  = require('webtask-tools');
 const app      = express();
 const Request  = require('request');
 const memoizer = require('lru-memoizer');
-const Error = require('nested-error-stacks');
+const NestedError = require('nested-error-stacks');
+const crypto   = require('crypto');
+
+function md5(str, raw) {
+	var hash = crypto.createHash('md5').update(str);
+	if (raw)
+		return hash.digest();
+	return hash.digest('hex');
+}
 
 function lastLogCheckpoint(req, res) {
   let ctx               = req.webtaskContext;
@@ -17,6 +25,17 @@ function lastLogCheckpoint(req, res) {
     return res.status(400).send({ message: 'Missing settings: ' + missing_settings.join(', ') });
   }
 
+  var secretKey;
+  try {
+    secretKey = fbadmin.credential.cert(JSON.parse(ctx.data.FIREBASE_SECRET_KEY));
+  } catch (err) {
+    console.log("Error parsing FIREBASE_SECRET_KEY json: ", err);
+    return callback(new NestedError('Error parsing FIREBASE_SECRET_KEY json: ', err));
+  }
+  fbadmin.initializeApp({
+    credential: secretKey
+  });
+  secretKey = null;
   // If this is a scheduled task, we'll get the last log checkpoint from the previous run and continue from there.
   req.webtaskContext.storage.get((err, data) => {
     if (err && err.output.statusCode !== 404) return res.status(err.code).send(err);
@@ -52,53 +71,62 @@ function lastLogCheckpoint(req, res) {
         getLogs({ checkpointId: startCheckpointId });
       },
       (context, callback) => {
-        const min_log_level = 1;
-        const log_matches_level = (log) => {
-          if (logTypes[log.type]) {
-            return logTypes[log.type].level >= min_log_level;
-          }
-          return true;
-        };
         // sdu successful user deletion
-        // sce successful email change
-        const types_filter = ['du', 'ss', 'sdu'];
-        const log_matches_types = (log) => {
-			console.log("Log: " + JSON.stringify(log));
-          return log.type && types_filter.indexOf(log.type) >= 0;
-        };
-
         context.logs = context.logs
-          //.filter(l => l.type !== 'sapi' && l.type !== 'fapi')
-          .filter(log_matches_level)
-          .filter(log_matches_types);
+          .filter(l => l.type === 'sdu' || l.type == 'ss');
 
         callback(null, context);
       },
       (context, callback) => {
-		console.log("Logs: " + context.logs.length);
+        console.log("Logs: " + context.logs.length);
         if (!context.logs.length) {
           return callback(null, context);
         }
 
-        const concurrent_calls = 1;
 
-        var secretKey;
-        try {
-          secretKey = fbadmin.credential.cert(JSON.parse(ctx.data.FIREBASE_SECRET_KEY));
-        } catch (err) {
-		  console.log("Error parsing FIREBASE_SECRET_KEY json: ", err);
-          return callback(new NestedError('Error parsing FIREBASE_SECRET_KEY json: ', err));
-        }
-        fbadmin.initializeApp({
-          credential: secretKey
-        });
-
-        async.eachLimit(context.logs, concurrent_calls, (log, cb) => {
+        context.userDeleteMatches = {};
+        var errors = [];
+        context.logs.forEach(log => {
           console.log("Log: ", log);
-          cb();
+          const userId = log.description.split(/:\s+/, 1)[1];
+          if (!userId) {
+            console.log("Missing description from log entry: ", log);
+            errors.push("Missing userId from description: " + log.description);
+          } else if (log.type === 'sdu') {
+            context.userDeleteMatches[userId] = true;
+          } else if (log.type === 'ss') {
+            delete context.userDeleteMatches[userId];
+          }
+        });
+        callback(err, context);
+      },
+      (context, callback) => {
+        const concurrent_calls = 3;
+        const errors = [];
+
+        if (context.userDeleteMatches.keys().length === 0)
+          return callback(null, context);
+
+        console.log("UserDeleteMatches: ", context.userDeleteMatches);
+        async.eachLimit(context.userDeleteMatches.keys(), concurrent_calls, function (userId, cb) {
+          userExists(userId, (err, email) => {
+            if (err) {
+              errors.push("" + err);
+            }
+            if (!email && !err) {
+              deleteFirebaseUser(userId, (err) => {
+                if (err) {
+                  errors.push("" + err);
+                }
+                cb();
+              });
+            }
+          });
         }, (err) => {
-			callback(err, context);
-		});
+          if (!err && errors) 
+            err = new Error(errors.join("; "));
+          callback(err, context);
+        });
       }
     ], (err, context) => {
       if (err) {
@@ -133,191 +161,37 @@ function lastLogCheckpoint(req, res) {
   });
 }
 
-const logTypes = {
-  's': {
-    event: 'Success Login',
-    level: 1 // Info
-  },
-  'seacft': {
-    event: 'Success Exchange',
-    level: 1 // Info
-  },
-  'seccft': {
-    event: 'Success Exchange (Client Credentials)',
-    level: 1 // Info
-  },
-  'feacft': {
-    event: 'Failed Exchange',
-    level: 3 // Error
-  },
-  'feccft': {
-    event: 'Failed Exchange (Client Credentials)',
-    level: 3 // Error
-  },
-  'f': {
-    event: 'Failed Login',
-    level: 3 // Error
-  },
-  'w': {
-    event: 'Warnings During Login',
-    level: 2 // Warning
-  },
-  'du': {
-    event: 'Deleted User',
-    level: 1 // Info
-  },
-  'fu': {
-    event: 'Failed Login (invalid email/username)',
-    level: 3 // Error
-  },
-  'fp': {
-    event: 'Failed Login (wrong password)',
-    level: 3 // Error
-  },
-  'fc': {
-    event: 'Failed by Connector',
-    level: 3 // Error
-  },
-  'fco': {
-    event: 'Failed by CORS',
-    level: 3 // Error
-  },
-  'con': {
-    event: 'Connector Online',
-    level: 1 // Info
-  },
-  'coff': {
-    event: 'Connector Offline',
-    level: 3 // Error
-  },
-  'fcpro': {
-    event: 'Failed Connector Provisioning',
-    level: 4 // Critical
-  },
-  'ss': {
-    event: 'Success Signup',
-    level: 1 // Info
-  },
-  'fs': {
-    event: 'Failed Signup',
-    level: 3 // Error
-  },
-  'cs': {
-    event: 'Code Sent',
-    level: 0 // Debug
-  },
-  'cls': {
-    event: 'Code/Link Sent',
-    level: 0 // Debug
-  },
-  'sv': {
-    event: 'Success Verification Email',
-    level: 0 // Debug
-  },
-  'fv': {
-    event: 'Failed Verification Email',
-    level: 0 // Debug
-  },
-  'scp': {
-    event: 'Success Change Password',
-    level: 1 // Info
-  },
-  'fcp': {
-    event: 'Failed Change Password',
-    level: 3 // Error
-  },
-  'sce': {
-    event: 'Success Change Email',
-    level: 1 // Info
-  },
-  'fce': {
-    event: 'Failed Change Email',
-    level: 3 // Error
-  },
-  'scu': {
-    event: 'Success Change Username',
-    level: 1 // Info
-  },
-  'fcu': {
-    event: 'Failed Change Username',
-    level: 3 // Error
-  },
-  'scpn': {
-    event: 'Success Change Phone Number',
-    level: 1 // Info
-  },
-  'fcpn': {
-    event: 'Failed Change Phone Number',
-    level: 3 // Error
-  },
-  'svr': {
-    event: 'Success Verification Email Request',
-    level: 0 // Debug
-  },
-  'fvr': {
-    event: 'Failed Verification Email Request',
-    level: 3 // Error
-  },
-  'scpr': {
-    event: 'Success Change Password Request',
-    level: 0 // Debug
-  },
-  'fcpr': {
-    event: 'Failed Change Password Request',
-    level: 3 // Error
-  },
-  'fn': {
-    event: 'Failed Sending Notification',
-    level: 3 // Error
-  },
-  'sapi': {
-    event: 'API Operation'
-  },
-  'limit_wc': {
-    event: 'Blocked Account',
-    level: 4 // Critical
-  },
-  'limit_ui': {
-    event: 'Too Many Calls to /userinfo',
-    level: 4 // Critical
-  },
-  'api_limit': {
-    event: 'Rate Limit On API',
-    level: 4 // Critical
-  },
-  'sdu': {
-    event: 'Successful User Deletion',
-    level: 1 // Info
-  },
-  'fdu': {
-    event: 'Failed User Deletion',
-    level: 3 // Error
-  },
-  'fapi': {
-    event: 'Failed API Operation',
-    level: 3 // Error
-  },
-  'limit_mu': {
-    event: 'Blocked IP Address',
-    level: 3 // Error
-  },
-  'slo': {
-    event: 'Success Logout',
-    level: 1 // Info
-  },
-  'flo': {
-    event: ' Failed Logout',
-    level: 3 // Error
-  },
-  'sd': {
-    event: 'Success Delegation',
-    level: 1 // Info
-  },
-  'fd': {
-    event: 'Failed Delegation',
-    level: 3 // Error
-  }
-};
+function deleteFirebaseUser(userId, cb) {
+  console.log("Delete Firbase User " + userId);
+  fbadmin.auth().deleteUser(userId)
+    .then(() => cb(null))
+    .catch(e => cb(e));
+}
+
+function userExits(userId, cb) {
+  var url = `https://${domain}/api/v2/users/${userId}`;
+
+  Request({
+    method: 'GET',
+    url: url,
+    json: true,
+    qs: {
+      fields: 'email'
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json'
+    }
+  }, (err, res, body) => {
+    if (err) {
+      console.log("Error getting user " + userId + ": ", err);
+      cb(null, false);
+    } else {
+      cb(null, true);
+    }
+  })
+
+}
 
 function getPageOfLogsFromAuth0 (domain, token, take, from, cb) {
   var url = `https://${domain}/api/v2/logs`;
